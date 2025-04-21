18a682ab49116dcc0e47f56320974881c19d439e
AlphaB135 <bngame2334@gmail.com>
2025-04-21 09:48:17 +0700

    chore: setup monorepo husky + lint-staged + ESLint config

---
+++ eslint.config.cjs
@@ -0,0 +1,11 @@
+// eslint.config.cjs
+module.exports = {
+    root: true,
+    env: { browser: true, node: true, es2022: true },
+    parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+    settings: { react: { version: "detect" } },
+    extends: [ "eslint:recommended", "plugin:react/recommended", "prettier" ],
+    plugins: ["react"],
+    rules: {}
+  }
+  
