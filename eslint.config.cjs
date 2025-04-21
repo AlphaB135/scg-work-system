de84fa3ff194f4b305cfed118203ea9cc3e61573
AlphaB135 <bngame2334@gmail.com>
2025-04-21 10:06:12 +0700

    chore: add .gitignore and fix ESLint flat config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,11 +1,25 @@
-// eslint.config.cjs
+// root/eslint.config.cjs
 module.exports = {
-    root: true,
-    env: { browser: true, node: true, es2022: true },
-    parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-    settings: { react: { version: "detect" } },
-    extends: [ "eslint:recommended", "plugin:react/recommended", "prettier" ],
+    env: {
+      browser: true,
+      node: true,
+      es2022: true
+    },
+    parserOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    settings: {
+      react: { version: "detect" }
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "prettier"
+    ],
     plugins: ["react"],
-    rules: {}
-  }
+    rules: {
+      // ใส่กฎเพิ่มเติมถ้าต้องการ
+    }
+  };
   
